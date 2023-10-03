0fec7b447c28b68050a08d1bb7849b502e90c0f1
Aman kumar <122856762+aman8115@users.noreply.github.com>
2023-10-03 22:58:32 +0530

    In this file solution of Question 6

---
+++ Question6.js
@@ -0,0 +1,7 @@
+const os = require("os")
+// find operating system name
+const opeSystem = os.type()
+console.log(" Operating System Name : " + opeSystem)
+// os release
+const relea = os.release()
+console.log( " Os release :" +  relea)
